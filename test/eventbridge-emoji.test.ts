8f3a3771f7949b6b1e6dbe7ae49dca1ca93e0126
Danielle Heberling <deeheber@users.noreply.github.com>
2023-09-22 19:19:04 -0700

    Initial commit

---
+++ test/eventbridge-emoji.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EventbridgeEmoji from '../lib/eventbridge-emoji-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eventbridge-emoji-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EventbridgeEmoji.EventbridgeEmojiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
